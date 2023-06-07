c3d7dea725cf09c1c33fd9dd66ac6588bc335877
Matheus Carvalho <henriquematheus.c@gmail.com>
2023-06-07 17:37:04 -0300

    Migrate SCSS to CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,19 @@
 const presetEnv = require('postcss-preset-env');
+const customMedia = require('postcss-custom-media');
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const atImport = require('postcss-import');
 
 module.exports = {
 	plugins: [
+		atImport({
+			path: ['./assets/css'],
+		}),
 		presetEnv({ stage: 0 }),
-		autoprefixer({
-			browsers: ['last 2 versions'],
+		autoprefixer(),
+		customMedia(),
+		cssnano({
+			preset: 'default',
 		}),
 	],
 };

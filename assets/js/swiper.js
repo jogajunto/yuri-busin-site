660f38bbd106354f9b0f76f9db19aff0c4b7a783
Matheus Carvalho <henriquematheus.c@gmail.com>
2023-06-14 11:35:34 -0300

    Fix the vendors folder structure

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,21 +1,18 @@
-import Swiper, { Navigation, A11y, Pagination, Autoplay } from 'swiper';
+import Swiper, { A11y } from 'swiper';
 
-Swiper.use([Navigation, A11y, Pagination, Autoplay]);
+// Configure Swiper to use modules
+Swiper.use([A11y]);
 
-var swiper = new Swiper('.mySwiper', {
+const swiperTranstornos = new Swiper('.swiper-transtornos', {
 	slidesPerView: 1.3,
 	spaceBetween: 24,
-  centeredSlides: true,
-  centeredSlidesBounds: true,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
+	centeredSlides: true,
+	centeredSlidesBounds: true,
+	breakpoints: {
+		768: {
+			slidesPerView: 4,
+			centeredSlidesBounds: false,
+			spaceBetween: 32,
+		},
 	},
-  breakpoints: {
-    768: {
-      slidesPerView: 4,
-      centeredSlidesBounds: false,
-	    spaceBetween: 32,
-    },
-  }
 });

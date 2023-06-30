1b7f1a2e82697f6771aeb557d99654a41c73af9d
Matheus Carvalho <henriquematheus.c@gmail.com>
2023-06-30 13:48:19 -0300

    Update time of homepage animation

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -8,51 +8,48 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const scenesElement = document.querySelector('.scenes');
+// Scroll Scenes
+const scenes = gsap.utils.toArray('.scene');
 
-if (scenesElement) {
-	// Scroll Scenes
-	const scenes = gsap.utils.toArray('.scene');
+// Set scenes height
+const scenesHeight = (scenes.length - 1) * 100 + '%';
 
-	// Set scenes height
-	const scenesHeight = (scenes.length - 1) * 100 + '%';
-
-	// Scenes Timeline
-	const scenesTimeline = gsap.timeline({
-		scrollTrigger: {
-			trigger: '.scenes__items',
-			pin: '.scenes',
-			start: 'top top',
-			end: `+=${scenesHeight}`,
-			scrub: 0.75,
-		},
-	});
-
-	// Set scenes wrapper to absolute
-	gsap.set(scenes, { position: 'absolute', top: 0 });
-
-	// ScrollTrigger for header pinning
-	ScrollTrigger.create({
-		trigger: '.scenes',
+// Scenes Timeline
+const scenesTimeline = gsap.timeline({
+	scrollTrigger: {
+		trigger: '.scenes__items',
+		pin: '.scenes',
 		start: 'top top',
-		endTrigger: '.scenes',
 		end: `+=${scenesHeight}`,
-		pin: 'header',
-	});
+		scrub: 0.75,
+	},
+});
 
-	// Loop over scenes
-	scenes.forEach(function (elem, i) {
-		if (i != 0) {
-			// Scene Enter animations
-			scenesTimeline.from(elem.querySelector('.scene__inner'), { autoAlpha: 0 }, i);
-		}
+// Set scenes wrapper to absolute
+gsap.set(scenes, { position: 'absolute', top: 0 });
 
-		// Scene Exit animations
-		if (i != scenes.length - 1) {
-			scenesTimeline.to(elem.querySelector('.scene__inner'), { autoAlpha: 0 }, i + 0.9);
-		}
-	});
+// ScrollTrigger for header pinning
+ScrollTrigger.create({
+	trigger: '.scenes',
+	start: 'top top',
+	endTrigger: '.scenes',
+	end: `+=${scenesHeight}`,
+	pin: 'header',
+});
 
-	// Make the duration of last scene longer
-	scenesTimeline.to(scenes[scenes.length - 1].querySelector('.scene__inner'), { autoAlpha: 1, duration: 1 });
-}
+// Loop over scenes
+scenes.forEach(function (elem, i) {
+	if (i != 0) {
+		// Scene Enter animations
+		scenesTimeline.from(elem.querySelector('.scene__inner'), { autoAlpha: 0 }, i);
+	}
+
+	// Scene Exit animations
+	if (i != scenes.length - 1) {
+		scenesTimeline.to(elem.querySelector('.scene__inner'), { autoAlpha: 0 }, i + 0.9);
+	}
+});
+
+// As first scene is already visible on page load, the second scene is taking more time to enter.
+// So we need to add a delay to the first scene exit animation.
+// scenesTimeline.to(scenes[0].querySelector('.scene__inner'), { autoAlpha: 0, duration: 0.5 }, 0.1);
